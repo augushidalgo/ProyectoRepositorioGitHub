addfc5a0536beaa9717e8d0276625ee808ec7622
Augusto Hidalgo <53703995+augushidalgo@users.noreply.github.com>
2025-05-06 11:33:08 -0600

    Demasiados mensajes

--- ProyectoRepositorioGitHub/Program.cs
+++ ProyectoRepositorioGitHub/Program.cs
@@ -5,11 +5,7 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Haciendo pruebas desde el repositorio local");
-            Console.WriteLine("Haciendo pruebas desde el repositorio local");
-            Console.WriteLine("Haciendo pruebas desde el repositorio local");
-            Console.WriteLine("Haciendo pruebas desde el repositorio local");
-            Console.WriteLine("Haciendo pruebas desde el repositorio local");
-            Console.WriteLine("Haciendo pruebas desde el repositorio local");
+
             Console.ReadKey();
         }
     }
